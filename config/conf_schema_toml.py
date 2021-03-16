9932fa29499b7c9adaf9f4798477dc4d7db825c0
Adrien-Pajot <pajot.adrien@wanadoo.fr>
2021-03-16 16:37:31 +0100

    configuration files modification

--- config/conf_schema_toml.py
+++ config/conf_schema_toml.py
@@ -9,6 +9,8 @@ from marshmallow import Schema, fields, validates_schema, ValidationError
 
 class GnModuleSchemaConf(Schema):
     AREA_TYPE = fields.List(fields.String(), missing=["COM", "M1", "M5", "M10"])
+    NB_CLASS_OBS = fields.Integer(missing=5)
+    NB_CLASS_TAX = fields.Integer(missing=5)
     BORNE_OBS = fields.List(fields.Integer(), missing=[1, 20, 40, 60, 80, 100, 120])
     BORNE_TAXON = fields.List(fields.Integer(), missing=[1, 5, 10, 15])
     SIMPLIFY_LEVEL = fields.Integer(missing=50)
