1f1d66e3edc533ea45a8fc47154b26de32b027a7
Adrien-Pajot <pajot.adrien@wanadoo.fr>
2021-03-23 16:24:05 +0100

    Refresh dashboard MV with CRON installed during the install_gn_module.py script. The CRON uses a geonature command

---
+++ backend/commands/geonature_cmd.py
@@ -0,0 +1,13 @@
+from flask.cli import with_appcontext
+
+from geonature.core.command import main
+
+from geonature.utils.env import DB
+
+@main.command()
+@with_appcontext
+def gn_dashboard_refresh_vm():
+    """
+       Rafraîchissement des VM du dashboard
+    """
+    DB.session.execute('SELECT gn_dashboard.refresh_materialized_view_data()')
