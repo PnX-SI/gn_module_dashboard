197ae710437ff95a3a4049cf17934e66b6928feb
jacquesfize <jacques.fize@ecrins-parcnational.fr>
2026-01-29 14:28:53 +0100

    apply black

--- backend/gn_module_dashboard/migrations/9a9ef04f6010_group3_inpn_vm_synthese.py
+++ backend/gn_module_dashboard/migrations/9a9ef04f6010_group3_inpn_vm_synthese.py
@@ -9,7 +9,6 @@ Create Date: 2023-09-06 14:29:46.848022
 from alembic import op
 import sqlalchemy as sa
 
-
 # revision identifiers, used by Alembic.
 revision = "9a9ef04f6010"
 down_revision = "cef2c2d66eb2"
@@ -20,8 +19,7 @@ depends_on = None
 def upgrade():
     op.execute("DROP MATERIALIZED VIEW gn_dashboard.vm_taxonomie")
     op.execute("DROP MATERIALIZED VIEW gn_dashboard.vm_synthese")
-    op.execute(
-        """
+    op.execute("""
     CREATE MATERIALIZED VIEW gn_dashboard.vm_synthese
     TABLESPACE pg_default
     AS SELECT s.id_synthese,
@@ -57,10 +55,8 @@ def upgrade():
 
     CREATE INDEX vm_synthese_cd_ref_idx ON gn_dashboard.vm_synthese USING btree (cd_ref);
     CREATE UNIQUE INDEX vm_synthese_id_synthese_idx ON gn_dashboard.vm_synthese USING btree (id_synthese);
-    """
-    )
-    op.execute(
-        """
+    """)
+    op.execute("""
         CREATE MATERIALIZED VIEW gn_dashboard.vm_taxonomie
     TABLESPACE pg_default
     AS SELECT 'Règne'::text AS level,
@@ -104,15 +100,13 @@ def upgrade():
     GROUP BY vm_synthese.group3_inpn
     WITH DATA;
     CREATE UNIQUE INDEX vm_taxonomie_name_taxon_level_idx ON gn_dashboard.vm_taxonomie USING btree (name_taxon, level);
-"""
-    )
+""")
 
 
 def downgrade():
     op.execute("DROP MATERIALIZED VIEW IF EXISTS gn_dashboard.vm_taxonomie")
     op.execute("DROP MATERIALIZED VIEW IF EXISTS gn_dashboard.vm_synthese")
-    op.execute(
-        """
+    op.execute("""
         CREATE MATERIALIZED VIEW gn_dashboard.vm_synthese
         TABLESPACE pg_default
         AS SELECT s.id_synthese,
@@ -147,10 +141,8 @@ def downgrade():
 
         CREATE INDEX vm_synthese_cd_ref_idx ON gn_dashboard.vm_synthese USING btree (cd_ref);
         CREATE UNIQUE INDEX vm_synthese_id_synthese_idx ON gn_dashboard.vm_synthese USING btree (id_synthese);
-               """
-    )
-    op.execute(
-        """
+               """)
+    op.execute("""
         CREATE MATERIALIZED VIEW gn_dashboard.vm_taxonomie
     TABLESPACE pg_default
     AS SELECT 'Règne'::text AS level,
@@ -190,5 +182,4 @@ def downgrade():
     WITH DATA;
 
     CREATE UNIQUE INDEX vm_taxonomie_name_taxon_level_idx ON gn_dashboard.vm_taxonomie USING btree (name_taxon, level);
-"""
-    )
+""")
--- backend/gn_module_dashboard/migrations/cef2c2d66eb2_declare_available_permissions.py
+++ backend/gn_module_dashboard/migrations/cef2c2d66eb2_declare_available_permissions.py
@@ -9,7 +9,6 @@ Create Date: 2023-06-08 16:46:03.951872
 from alembic import op
 import sqlalchemy as sa
 
-
 # revision identifiers, used by Alembic.
 revision = "cef2c2d66eb2"
 down_revision = "488a2eeee4be"
@@ -18,8 +17,7 @@ depends_on = ("f051b88a57fd",)
 
 
 def upgrade():
-    op.execute(
-        """
+    op.execute("""
         INSERT INTO
             gn_permissions.t_permissions_available (
                 id_module,
@@ -45,10 +43,8 @@ def upgrade():
             gn_permissions.t_objects o ON o.code_object = v.object_code
         JOIN
             gn_permissions.bib_actions a ON a.code_action = v.action_code
-        """
-    )
-    op.execute(
-        """
+        """)
+    op.execute("""
         WITH bad_permissions AS (
             SELECT
                 p.id_permission
@@ -74,13 +70,11 @@ def upgrade():
                 USING bad_permissions bp
         WHERE
             bp.id_permission = p.id_permission;
-        """
-    )
+        """)
 
 
 def downgrade():
-    op.execute(
-        """
+    op.execute("""
         DELETE FROM
             gn_permissions.t_permissions_available pa
         USING
@@ -89,5 +83,4 @@ def downgrade():
             pa.id_module = m.id_module
             AND
             module_code = 'DASHBOARD'
-        """
-    )
+        """)
