6b405256f13efa3838201f360f01964f7eb3c797
Christophe Ramet <christophe.ramet@mnhn.fr>
2026-02-27 12:03:57 +0100

    chore: Updated a function to be compatible with setuptools 82

--- backend/gn_module_dashboard/migrations/2628978e1016_add_dashboard_schema.py
+++ backend/gn_module_dashboard/migrations/2628978e1016_add_dashboard_schema.py
@@ -8,7 +8,7 @@ Create Date: 2022-08-25 14:19:25.865116
 
 from alembic import op
 import sqlalchemy as sa
-import pkg_resources
+from importlib.resources import files
 
 # revision identifiers, used by Alembic.
 revision = "2628978e1016"
@@ -21,9 +21,8 @@ schema = "gn_dashboard"
 
 def upgrade():
     sql_file = "dashboard.sql"
-    operations = pkg_resources.resource_string(
-        "gn_module_dashboard.migrations", f"data/{sql_file}"
-    ).decode("utf-8")
+    resource_file = files("gn_module_dashboard.migrations") / "data" / sql_file
+    operations = resource_file.read_text(encoding="utf-8")
     op.execute(operations)
 
 
